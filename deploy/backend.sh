8c23909a9de373a8e6f8bcef0b0f4ed378408764
satz007 <satheesh@statwig.com>
2020-08-28 20:34:08 +0000

    Added autodeploy script

---
+++ deploy/backend.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+# any future command that fails will exit the script
+python2.7  $HOME/PRD/current/theledger/deploy/aws.py start
+
+set -e
+
+echo "home is" $HOME
+BASE_DIR=$HOME/PRD
+if [ -d "$BASE_DIR/releases" ]; then
+  ### Take action if $DIR exists ###
+  echo "Already exists..."
+else
+  mkdir -p $HOME/PRD/releases
+fi
+
+if [ -d "$BASE_DIR/temp" ]; then
+  ### Take action if $DIR exists ###
+  echo "Already exists..."
+else
+ mkdir -p $HOME/PRD/temp
+fi
+
+if [ -d "$BASE_DIR/current" ]; then
+  ### Take action if $DIR exists ###
+  echo "Already exists..."
+else
+mkdir -p $HOME/PRD/current
+fi
+
+if [ -d "$BASE_DIR/current/theledger" ]; then
+
+mv $HOME/PRD/current/theledger $HOME/PRD/temp/
+
+fi
+
+# clone the repo again
+cd $HOME/PRD/current
+git clone https://gitlab.com/statwig-public/theledger.git
+cd $HOME/PRD/current/theledger
+
+git checkout autodeploy_latest
+echo "Starting instances"
+cd $HOME/PRD/current/theledger
+#Kill all the running process
+#pm2 kill
+sh /home/ubuntu/predeployscripts/pre-deploy-test.sh
+#cd backend
+#Starting the new deploy servicess
+#cd -P .
+ #  for dir in ./*/
+  #     do cd -P "$dir" ||continue
+  #    echo "curr dir $PWD"
+   #    printf %s\\n "$PWD" >&2
+    #   npm install && pm2 start && cd "$OLDPWD" ||
+    #! break; done || ! cd - >&2
+sh stop.sh
+./deploy.sh TEST ALL
+systemctl start nginx
+#pms stop 8 (tx service)
+    
+if [ -d "$BASE_DIR/temp/theledger" ];then
+
+mv $HOME/PRD/temp/theledger $HOME/PRD/releases/theledger_`date '+%Y%m%d'`
+
+fi
+
---
+++ deploy/frontend.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+#python2.7  $HOME/theledger/deploy/aws.py start
+
+# any future command that fails will exit the script
+set -e
+
+# Delete the old repos
+rm -rf $HOME/theledger
+
+# clone the repo again
+git clone https://gitlab.com/statwig-public/theledger.git
+cd ./theledger
+
+#git checkout development
+git checkout autodeploy_latest
+cd $HOME/theledger/frontend
+export PATH=$PATH:$HOME/node/bin
+npm install
+ENVIRONMENT=test npm run build
+echo "Uploading the files"
+echo "$pwd"
+chmod 600 $HOME/keys/testdevserver.pem
+#testdevserverttt
+scp -r -i $HOME/keys/testdevserver.pem /home/ubuntu/theledger/frontend/dist  ubuntu@ec2-13-232-141-136.ap-south-1.compute.amazonaws.com:/home/ubuntu/build_files
+scp -r -i $HOME/keys/testdevserver.pem /home/ubuntu/theledger/frontend/index.html  ubuntu@ec2-13-232-141-136.ap-south-1.compute.amazonaws.com:/home/ubuntu/build_files/
+
+python2.7  $HOME/theledger/deploy/aws.py stop
