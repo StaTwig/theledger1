32925b7aad87fc042ee738a6771c6ed3b332c493
Ashwini Shankar Patankar <ashwinispatankar@gmail.com>
2021-05-03 20:26:39 +0530

    CICD testing is going on

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,9 @@
 #i Lets write the public key of our aws instance
 
 eval $(ssh-agent -s)
+echo "The chabi of the lock : "
 echo "$PRIVATE_KEY"
+echo "have you seen the chabi"
 echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 
